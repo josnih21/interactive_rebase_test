83ee0ddd189b25754bfc9d7e058eb4a5d38469e7
josnih <josnih@gmail.com>
2020-12-19 18:19:33 +0000

    init commit :sparkles:

---
+++ android/app/src/main/kotlin/com/example/test_interactive_rebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_interactive_rebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('This is just a test of git interactive rebase'),
+      ),
+    );
+  }
+}
